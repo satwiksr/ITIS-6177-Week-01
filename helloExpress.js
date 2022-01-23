9b551c0bf23c7c21b97ced5eb4683d8828269c77
Satwik Rao <satwikrao.sr@gmail.com>
2022-01-22 19:48:41 -0500

    Express file commit

---
+++ helloExpress.js
@@ -0,0 +1,14 @@
+var express = require('express')
+var app = express()
+var port = 3000
+
+app.get("/", function(req, res){
+    res.send('Hello World!')
+})
+
+app.listen(port, function(error) {
+    if(!error) {
+        console.log('server is listening on port ' + port)
+    }
+
+})
